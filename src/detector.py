f1ed7dd603ace84b1b8015c2d7d57515d9de3947
jarrey <jar.bob@gmail.com>
2015-07-06 20:02:40 +0800

    Fix error in RaspberryPi environment <numpy type error>.

--- src/detector.py
+++ src/detector.py
@@ -9,7 +9,7 @@ def detectCircle(imagePath):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.Canny(gray, 32, 2)
     cv2.imwrite("canny.jpg", gray)
-    circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 50, 10, 50, 6, 10)
+    circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 10, np.array([]), 10, 20, 6, 20)
 
     if circles is not None:
         circles = np.uint16(np.around(circles)) 
