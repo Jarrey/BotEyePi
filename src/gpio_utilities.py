6570e806b40c74c229cdf6b4269ea202224a9701
Jarrey <jar.bob@gmail.com>
2015-09-08 16:44:01 +0800

    Change code for GPIO

--- src/gpio_utilities.py
+++ src/gpio_utilities.py
@@ -7,20 +7,19 @@ import time
 def getGPIOInput(pin, delay = 0):
     try:
         i = -1
-        value = 0 
+        o = GPIO.input(pin)
         while i < delay:
-            value = GPIO.input(pin)
+            v = GPIO.input(pin)
             time.sleep(0.01)
             i = i + 1
-        return value
+        return int(v) if v == o else int(not v)
     except:
         raise
             
 
 if __name__ == '__main__':
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)    
     GPIO.setup(14, GPIO.IN)
     while True:
-        # print "original: ", GPIO.input(14)
-        print "new: ", getGPIOInput(14, 10)
+        print getGPIOInput(14, 10)
