2e0ea0233800757ccb2155b6d06ac21b69d1b0ae
jarrey <jar.bob@gmail.com>
2015-09-08 08:40:29 +0800

    Change build script to include gpio utilities

--- src/build.py
+++ src/build.py
@@ -2,7 +2,7 @@
 import py_compile as c
 import os
 
-files = ['run.py', 'bar.py', 'capturer.py', 'logger.py', 'power_monitor.py', 'setup_camera.py']
+files = ['run.py', 'bar.py', 'capturer.py', 'logger.py', 'power_monitor.py', 'setup_camera.py', 'gpio_utilities.py']
 
 for f in files:
 	c.compile(f)
